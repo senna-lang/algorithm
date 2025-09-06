e2b774fd1e9ca1c625f4fa0cce49a4eea4d10341
senna-lang <asrsena3302@gmail.com>
2025-09-06 12:52:57 +0900

    refactor: optimize recursiveSubsetSum and add memoization for tribo function

--- note/recursive/main.py
+++ note/recursive/main.py
@@ -10,10 +10,10 @@ def GCD(m: int, n: int):
 
 # print(GCD(51, 15))
 
-
 memo = {}
 
 
+# 0(N)
 def fibo(num: int):
     if num == 0:
         return 0
@@ -30,8 +30,15 @@ def fibo(num: int):
 
 # print(fibo(6))
 
+recursiveSubsetSum_memo = {}
+
+
 # 0(2^N)
 def recursiveSubsetSum(arr: List[int], w: int, num: int):
+    key = (w, num)
+    if key in recursiveSubsetSum_memo:
+        return recursiveSubsetSum_memo[key]
+
     if num == 0:
         if w == 0:
             return True
@@ -39,12 +46,37 @@ def recursiveSubsetSum(arr: List[int], w: int, num: int):
             return False
 
     if recursiveSubsetSum(arr, w, num - 1):
+        recursiveSubsetSum_memo[key] = True
         return True
     if recursiveSubsetSum(arr, w - arr[len(arr) - 2], num - 1):
+        recursiveSubsetSum_memo[key] = True
         return True
 
+    recursiveSubsetSum_memo[key] = False
     return False
 
 
 arr = [0, 1, 2]
-print(recursiveSubsetSum(arr, 1, len(arr)))
+# print(recursiveSubsetSum(arr, 4, len(arr)))
+
+tribo_memo = {}
+
+
+# O(N)
+def tribo(num: int):
+    if num == 0:
+        return 0
+    if num == 1:
+        return 0
+    if num == 2:
+        return 1
+
+    if num in tribo_memo:
+        return tribo_memo[num]
+
+    tribo_memo[num] = tribo(num - 1) + tribo(num - 2) + tribo(num - 3)
+
+    return tribo_memo[num]
+
+
+# print(tribo(6))
