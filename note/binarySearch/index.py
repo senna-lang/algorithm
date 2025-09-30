691e10d56054dabed5a34b5f2cb4bed5e612ae0b
senna-lang <asrsena3302@gmail.com>
2025-09-30 22:35:46 +0900

    generalized binary search

--- note/binarySearch/index.py
+++ note/binarySearch/index.py
@@ -13,4 +13,26 @@ def binarySearch(arr, key):
             right = mid - 1
         elif mid <= key:
             left = mid + 1
-    return -1
+    return
+
+
+# x が条件を満たすかどうか
+def P(x):
+    # ここに条件を実装
+    pass
+
+
+# P(x) = True となる最小の整数 x を返す
+def binary_search():
+    left, right = 0, 0
+
+    while right - left > 1: # 最終的には１つの要素になってright(1) - left(0) = 1 となるため
+        mid = left + (right - left) // 2
+        # 条件が真なら答えは左にあるため右を切り捨て
+        # 偽なら答えは右にあるため左を切り捨て
+        if P(mid):
+            right = mid
+        else:
+            left = mid
+
+    return right
