2f498e5199db8a1d4c64a186726c07e05411fbc0
senna-lang <asrsena3302@gmail.com>
2025-10-04 12:22:33 +0900

    bisectBinarySearch

--- note/binarySearch/index.py
+++ note/binarySearch/index.py
@@ -1,3 +1,8 @@
+import bisect
+
+INF = 20000000  # 十分大きな値
+
+
 def binarySearch(arr, key):
     n = len(arr)
 
@@ -23,10 +28,12 @@ def P(x):
 
 
 # P(x) = True となる最小の整数 x を返す
-def binary_search():
+def binarySearch2():
     left, right = 0, 0
 
-    while right - left > 1: # 最終的には１つの要素になってright(1) - left(0) = 1 となるため
+    while (
+        right - left > 1
+    ):  # 最終的には１つの要素になってright(1) - left(0) = 1 となるため
         mid = left + (right - left) // 2
         # 条件が真なら答えは左にあるため右を切り捨て
         # 偽なら答えは右にあるため左を切り捨て
@@ -36,3 +43,31 @@ def binary_search():
             left = mid
 
     return right
+
+
+def bisectBinarySearch():
+    # 入力を受け取る
+    N, K = map(int, input().split())
+    a = list(map(int, input().split()))
+    b = list(map(int, input().split()))
+
+    # 暫定最小値を格納する変数
+    min_value = INF
+
+    # b をソート
+    b.sort()
+
+    # a を固定して解く
+    for i in range(N):
+        # b の中で K - a[i] 以上の範囲での最小値を示すインデックス
+        pos = bisect.bisect_left(b, K - a[i])
+
+        # インデックスが範囲内かチェック
+        if pos < N:
+            val = b[pos]
+
+            # min_value と比較する
+            if a[i] + val < min_value:
+                min_value = a[i] + val
+
+    print(min_value)
